a1ec4a7915d1b540c57bf52b6e1b2acfa67aea3f
chrispaganon <info@chrispaganon.com>
2024-05-21 14:31:40 -0400

    Prototype query content

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>HI</h1>
+  <div>
+    <ContentRenderer v-if="data" :content="data">
+      <h3>{{ data.title }}</h3>
+      <ContentRendererMarkdown :value="data" />
+    </ContentRenderer>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { data } = await useAsyncData('home', () =>
+  queryContent('/en/portfolio-intro').findOne()
+);
+</script>
