14581527a5922591deac56f8b907a0561fe3f0c9
chrispaganon <info@chrispaganon.com>
2024-05-21 14:47:45 -0400

    Add services table

---
+++ components/ServicesTable.vue
@@ -0,0 +1,98 @@
+<template>
+  <section class="services-section content-section">
+    <h2>{{ $t('ServicesTable.heading') }}</h2>
+    <p>
+      {{ $t('ServicesTable.sub-heading.part1') }}
+      <strong>{{ $t('ServicesTable.sub-heading.bold') }}</strong>
+      {{ $t('ServicesTable.sub-heading.part2') }}
+    </p>
+    <div v-if="services" class="services-wrapper">
+      <div
+        v-for="service in services"
+        :key="service.id"
+        class="service-wrapper"
+      >
+        <ContentRenderer :content="service">
+          <div class="service-title">
+            <p class="icon">{{ service.icon }}</p>
+            <p>{{ service.title }}</p>
+          </div>
+          <ContentRendererMarkdown :value="service" />
+        </ContentRenderer>
+      </div>
+    </div>
+    <p>
+      <em>{{ $t('ServicesTable.note') }}</em>
+    </p>
+    <NuxtLink
+      :to="localePath({ path: '/#contact-me' })"
+      class="contact-me-btn button"
+    >
+      {{ $t('NavBar.contact-me') }}
+    </NuxtLink>
+  </section>
+</template>
+
+<script setup lang="ts">
+const { data: services } = await useAsyncData('en-services', () =>
+  queryContent('en', 'services').find()
+);
+</script>
+
+<style scoped>
+.services-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 60px auto 40px auto;
+  border: 1px solid gray;
+  border-radius: 15px;
+}
+.service-wrapper {
+  width: 33%;
+  text-align: center;
+}
+.service-wrapper:not(:last-child) {
+  border-right: 1px solid gray;
+}
+
+.service-title {
+  padding: 20px 20px 5px 20px;
+  margin: auto;
+}
+.service-title p {
+  font-weight: 600;
+}
+:deep(.nuxt-content ul) {
+  padding: 5px 20px 20px 20px;
+  font-size: 19px;
+}
+:deep(.nuxt-content li) {
+  margin: 10px 0;
+}
+.icon {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+
+@media (max-width: 850px) {
+  .service-wrapper {
+    width: 100%;
+  }
+  .service-wrapper:not(:last-child) {
+    border-right: none;
+    border-bottom: 1px solid gray;
+  }
+}
+
+.button {
+  margin: 60px auto 20px auto;
+  font-size: 20px;
+  border-radius: 30px;
+}
+@media (max-width: 450px) {
+  .button {
+    font-size: 18px;
+    padding: 15px 25px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,93 @@
 <template>
-  <div>
-    <ContentRenderer v-if="data" :content="data">
-      <h3>{{ data.title }}</h3>
-      <ContentRendererMarkdown :value="data" />
-    </ContentRenderer>
-  </div>
+  <main>
+    <section class="hero-banner">
+      <h1>{{ $t('homepage.heading1') }}</h1>
+    </section>
+    <div class="home-content-wrapper">
+      <section class="introduction content-section">
+        <p>{{ $t('homepage.intro') }}</p>
+        <img src="/images/me.jpg" alt="me" />
+      </section>
+      <hr class="separator" />
+      <ServicesTable />
+    </div>
+  </main>
 </template>
 
-<script setup lang="ts">
-const { data } = await useAsyncData('home', () =>
-  queryContent('/en/portfolio-intro').findOne()
-);
-</script>
+<style scoped>
+.content-section {
+  margin: auto;
+  max-width: 1024px;
+  padding: 60px 40px;
+}
+.background-section-wrapper {
+  width: 100%;
+  background-color: rgb(255, 251, 246);
+}
+
+.introduction {
+  position: relative;
+  padding: 150px 40px;
+}
+.introduction p {
+  font-size: 2em;
+  margin-right: 170px;
+  margin-bottom: 0;
+}
+img {
+  position: absolute;
+  top: -75px;
+  right: 20px;
+  width: 200px;
+  border-radius: 50%;
+  margin: 0 10px;
+  transition: transform 500ms ease-out;
+}
+img:hover {
+  transform: translateY(-10px) scale(1.1, 1.1);
+}
+
+.separator {
+  border-top: 2px solid gray;
+  max-width: 66%;
+}
+.services-section {
+  text-align: center;
+}
+
+@media (max-width: 925px) {
+  .introduction {
+    padding: 140px 40px 100px 40px;
+  }
+  .introduction p {
+    font-size: 1.8em;
+    margin-right: 0;
+    text-align: center;
+  }
+  img {
+    top: -90px;
+  }
+}
+@media (max-width: 600px) {
+  img {
+    width: 170px;
+    margin: 0;
+    top: -75px;
+    right: calc(50% - 170px / 2);
+  }
+}
+@media (max-width: 450px) {
+  .content-section {
+    padding: 50px 20px;
+  }
+  .introduction p {
+    font-size: 1.5em;
+    margin: 30px 0 10px 0;
+  }
+  img {
+    width: 150px;
+    top: -120px;
+    right: calc(50% - 150px / 2);
+  }
+}
+</style>
