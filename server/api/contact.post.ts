596eaa72c9cbef9e762f5a6a0f9d030e243a7a59
chrispaganon <info@chrispaganon.com>
2024-05-22 15:08:00 -0400

    Add server side validation for form

---
+++ server/api/contact.post.ts
@@ -0,0 +1,40 @@
+export default eventHandler(async (event) => {
+  const body = await readBody(event);
+  console.log(body);
+
+  const name = body.name;
+  if (!name || name.length === 0 || typeof name !== 'string') {
+    throw createError({
+      statusCode: 400,
+      message: 'Name is required',
+    });
+  }
+
+  const email = body.email;
+  if (!email || email.length === 0 || typeof email !== 'string') {
+    throw createError({
+      statusCode: 400,
+      message: 'Email is required',
+    });
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    throw createError({
+      statusCode: 400,
+      message: 'Email is invalid',
+    });
+  }
+
+  const description = body.description;
+  if (
+    !description ||
+    description.length === 0 ||
+    typeof description !== 'string'
+  ) {
+    throw createError({
+      statusCode: 400,
+      message: 'Description is required',
+    });
+  }
+
+  return 'Hello, World!';
+});
