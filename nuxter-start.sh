c7291060cd6c1c28168793b21d4d1f5c406d2538
chrispaganon <info@chrispaganon.com>
2025-09-21 13:05:17 +0200

    migrate to traefik to

--- nuxter-start.sh
+++ nuxter-start.sh
@@ -2,8 +2,5 @@ projectName="chrispaganon"
 projectNameProduction="$projectName-production"
 projectNameStaging="$projectName-staging"
 
-docker network create ${projectNameProduction}_caddy_nuxt_app
-docker network create ${projectNameStaging}_caddy_nuxt_app
-
 docker compose -f ./nuxter-config/nuxter-base-compose.yml --env-file ./nuxter-config/production.env -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
 docker compose -f ./nuxter-config/nuxter-base-compose.yml --env-file ./nuxter-config/staging.env -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
