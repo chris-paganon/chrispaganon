38233796be947327c4a97a29dc913c75fd93b902
chrispaganon <info@chrispaganon.com>
2025-10-12 16:35:39 +0200

    add nuxter start to deploy script

--- nuxter-deploy.sh
+++ nuxter-deploy.sh
@@ -4,3 +4,5 @@ rsync -avzre 'ssh' docker-compose.yml Dockerfile nuxter-start.sh \
   .env.production .env.staging \
   .output.production .output.staging \
   nuxter@77.37.87.199:~/nuxter/chrispaganon
+
+ssh nuxter@77.37.87.199 "cd ~/nuxter/chrispaganon && ./nuxter-start.sh"
