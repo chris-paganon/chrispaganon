7887fe276a9d346e7b3c92d7838b9d108f7c19e0
chrispaganon <info@chrispaganon.com>
2025-10-13 13:52:58 +0200

    better deploy scripts

--- nuxter-deploy.sh
+++ nuxter-deploy.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-rsync -avzre 'ssh' docker-compose.yml Dockerfile nuxter-start.sh \
+REMOTE_PATH="~/nuxter/chrispaganon"
+REMOTE_SSH="nuxter@77.37.87.199"
+
+MAIN_FILES="docker-compose.yml Dockerfile nuxter-start.sh"
+
+if [[ "$*" == *"--production"* ]]; then
+    echo "Deploying production environment..."
+    rsync -azre 'ssh' ${MAIN_FILES} \
+      .env.production \
+      .output.production \
+      ${REMOTE_SSH}:${REMOTE_PATH}
+    
+    ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh --production"
+    exit 0
+fi
+
+if [[ "$*" == *"--staging"* ]]; then
+    echo "Deploying staging environment..."
+
+    rsync -azre 'ssh' ${MAIN_FILES} \
+      .env.staging \
+      .output.staging \
+      ${REMOTE_SSH}:${REMOTE_PATH}
+
+    ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh --staging"
+    exit 0
+fi
+
+echo "Deploying production and staging environments..."
+
+rsync -azre 'ssh' ${MAIN_FILES} \
   .env.production .env.staging \
   .output.production .output.staging \
-  nuxter@77.37.87.199:~/nuxter/chrispaganon
+  ${REMOTE_SSH}:${REMOTE_PATH}
 
-ssh nuxter@77.37.87.199 "cd ~/nuxter/chrispaganon && ./nuxter-start.sh"
+ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh"
--- nuxter-start.sh
+++ nuxter-start.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-projectName="chrispaganon"
-projectNameProduction="$projectName-production"
-projectNameStaging="$projectName-staging"
+PROJECT_NAME="chrispaganon"
 
-# Check if --local flag is provided
+PROJECT_NAME_PRODUCTION="$PROJECT_NAME-production"
+PROJECT_NAME_STAGING="$PROJECT_NAME-staging"
+
+function start_production() {
+    docker compose -f ./docker-compose.yml --env-file ./.env.production -p ${PROJECT_NAME_PRODUCTION} --project-directory ./ up --build --remove-orphans -d
+}
+
+function start_staging() {
+    docker compose -f ./docker-compose.yml --env-file ./.env.staging -p ${PROJECT_NAME_STAGING} --project-directory ./ up --build --remove-orphans -d
+}
+
 if [[ "$*" == *"--local"* ]]; then
     echo "Starting in local mode..."
-    docker compose -f ./docker-compose.yml -f ./docker-compose.override.yml --env-file ./.env.staging -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
-    docker compose -f ./docker-compose.yml -f ./docker-compose.override.yml --env-file ./.env.production -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
-else
-    echo "Starting production and staging environments..."
-    docker compose -f ./docker-compose.yml --env-file ./.env.production -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
-    docker compose -f ./docker-compose.yml --env-file ./.env.staging -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
+    docker compose -f ./docker-compose.yml -f ./docker-compose.override.yml --env-file ./.env.staging -p ${PROJECT_NAME_PRODUCTION} --project-directory ./ up --build --remove-orphans -d
+    docker compose -f ./docker-compose.yml -f ./docker-compose.override.yml --env-file ./.env.production -p ${PROJECT_NAME_STAGING} --project-directory ./ up --build --remove-orphans -d
+    exit 0
 fi
+
+if [[ "$*" == *"--production"* ]]; then
+    echo "Starting production environment..."
+    start_production
+    exit 0
+fi
+
+if [[ "$*" == *"--staging"* ]]; then
+    echo "Starting staging environment..."
+    start_staging
+    exit 0
+fi
+
+echo "Starting production and staging environments..."
+start_production
+start_staging
