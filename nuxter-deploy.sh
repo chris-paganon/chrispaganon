5695f38a33d2cd244249564e38a76322fd6a0c0e
chrispaganon <info@chrispaganon.com>
2025-10-13 19:04:17 +0200

    simplify nuxter deploy

--- nuxter-deploy.sh
+++ nuxter-deploy.sh
@@ -1,74 +1,47 @@
 #!/bin/bash
 
 PROJECT_NAME="chrispaganon"
-PROJECT_NAME_STAGING="$PROJECT_NAME-staging"
-PROJECT_NAME_PRODUCTION="$PROJECT_NAME-production"
-
 REMOTE_PATH="~/nuxter/chrispaganon"
 REMOTE_SSH="nuxter@77.37.87.199"
-
 MAIN_FILES="docker-compose.yml Dockerfile nuxter-start.sh"
 
-if [[ "$*" == *"--production"* ]]; then
-  echo "Building production images..."
+deploy_environment() {
+  local env=$1
+  local project_name="${PROJECT_NAME}-${env}"
+  local env_file=".env.${env}"
+  local images_dir="docker-images-${env}"
+  
+  echo "Building ${env} images..."
   docker compose -f ./docker-compose.yml \
-    --env-file ./.env.production -p ${PROJECT_NAME_PRODUCTION} --project-directory ./ \
+    --env-file ./${env_file} -p ${project_name} --project-directory ./ \
     build
 
-  # Extract image names and save them to a file for the next step
   echo "Extracting built image names..."
-  images=$(docker images --format "table {{.Repository}}" | grep "${PROJECT_NAME_PRODUCTION}")
+  images=$(docker images --format "table {{.Repository}}" | grep "${project_name}")
 
-  echo "Saving images to docker-images-production directory..."
-  mkdir -p docker-images-production
+  echo "Saving images to ${images_dir} directory..."
+  mkdir -p ${images_dir}
   echo "$images" | while read -r image; do
     echo "Saving $image..."
-    docker image save "$image" --output "docker-images-production/${image}.tar"
+    docker image save "$image" --output "${images_dir}/${image}.tar"
   done
 
-  echo "Deploying production environment..."
-  rsync -Pazre 'ssh' ${MAIN_FILES} \
-    .env.production \
-    docker-images-production \
-    ${REMOTE_SSH}:${REMOTE_PATH}
+  echo "Deploying ${env} environment..."
+  rsync -Pazre 'ssh' ${MAIN_FILES} ${env_file} ${images_dir} ${REMOTE_SSH}:${REMOTE_PATH}
   
-  ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh --production"
+  ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh --${env}"
+}
+
+if [[ "$*" == *"--production"* ]]; then
+  deploy_environment "production"
   exit 0
 fi
 
 if [[ "$*" == *"--staging"* ]]; then
-  echo "Building staging images..."
-  docker compose -f ./docker-compose.yml \
-    --env-file ./.env.staging -p ${PROJECT_NAME_STAGING} --project-directory ./ \
-    build
-
-  # Extract image names and save them to a file for the next step
-  echo "Extracting built image names..."
-  images=$(docker images --format "table {{.Repository}}" | grep "${PROJECT_NAME_STAGING}")
-
-  echo "Saving images to docker-images-staging directory..."
-  mkdir -p docker-images-staging
-
-  echo "$images" | while read -r image; do
-    echo "Saving $image..."
-    docker image save "$image" --output "docker-images-staging/${image}.tar"
-  done
-
-  echo "Deploying staging environment..."
-  rsync -Pazre 'ssh' ${MAIN_FILES} \
-    .env.staging \
-    docker-images-staging \
-    ${REMOTE_SSH}:${REMOTE_PATH}
-
-  ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh --staging"
+  deploy_environment "staging"
   exit 0
 fi
 
 echo "Deploying production and staging environments..."
-
-rsync -Pazre 'ssh' ${MAIN_FILES} \
-  .env.production .env.staging \
-  docker-images-production docker-images-staging \
-  ${REMOTE_SSH}:${REMOTE_PATH}
-
-ssh ${REMOTE_SSH} "cd ${REMOTE_PATH} && ./nuxter-start.sh"
+deploy_environment "production"
+deploy_environment "staging"
