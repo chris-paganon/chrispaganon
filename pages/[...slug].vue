51759f86c0c97c14ab463c5fac806b7a8495c814
chrispaganon <info@chrispaganon.com>
2025-11-02 15:10:05 +0100

    add 404 page

---
+++ pages/[...slug].vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div>
+      <h1>404, this page doesn't exist</h1>
+      <NuxtLink :to="localePath({ name: 'index' })" class="button">
+        Go back to the home page
+      </NuxtLink>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+const localePath = useLocalePath();
+</script>
+
+<style scoped>
+main {
+  min-height: 100vh;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+a {
+  margin: 40px auto;
+}
+</style>
