53126220a54a3df2ba3b3f2aeafa74053907cb61
Chris Paganon <info@chrispaganon.com>
2026-03-12 19:14:18 +0100

    add framer motion library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/i18n', '@nuxt/eslint'],
+  modules: ['@nuxt/content', '@nuxtjs/i18n', '@nuxt/eslint', 'motion-v/nuxt'],
   app: {
     head: {
       title: 'Chris Paganon',
